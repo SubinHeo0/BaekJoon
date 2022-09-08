812cd526bc163ace579233eb0c43ace87c641451
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-09 01:29:26 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.java
    코드 간결화

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -4,21 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] original = {1,1,2,2,2,8};
+        int[] original = { 1, 1, 2, 2, 2, 8 };
         int[] input = new int[original.length];
-        int[] result = new int[original.length];
 
-        String str = scanner.nextLine();
-        String[] strArr = str.split(" ");
-        
-        for(int i=0; i<strArr.length; i++){
-            input[i] = Integer.valueOf(strArr[i]);
-            result[i] = original[i] - input[i];
-        }
-
-        
-        for(int i=0; i<original.length; i++){
-            System.out.print(result[i] + " ");
+        for (int i = 0; i < original.length; i++) {
+            input[i] = scanner.nextInt();
+            System.out.print((original[i] - input[i]) + " ");
         }
 
         scanner.close();
