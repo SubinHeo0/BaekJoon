c36640e1e04ca7f5e7448c0a13faf5bffd3aa1a7
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-08 15:53:38 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String original = scanner.nextLine();
+
+        System.out.println(original + "??!");
+
+        scanner.close();
+
+    }
+}
