bba7e36810122a6fffa2ba5807cc63cca5f2e21c
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-08 15:23:02 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+       
+        // 1
+        //System.out.println("Hello World!");
+
+        // 2
+        // String str = "강한친구 대한육군";
+        // for(int i=0; i<2; i++){
+        //     System.out.println(str);
+        // }
+
+        // 3
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // System.out.println(a+b);
+
+        // 4
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // System.out.println(a-b);
+
+        // 5
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // System.out.println(a*b);
+        
+        // 6
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a/(double)b);
+
+
+
+
+
+
+
+
+
+        scanner.close();
+
+
+    }
+}
