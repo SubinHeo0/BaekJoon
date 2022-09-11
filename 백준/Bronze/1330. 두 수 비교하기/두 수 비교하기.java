1b70c643381cdfcbcc826889729b635898920fac
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-12 01:07:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,26 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         if (a > b) {
-            System.out.println(">");
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
         }
 
-        if (a < b) {
-            System.out.println("<");
-        }
-
-        if (a == b) {
-            System.out.println("==");
-        }
-
-        sc.close();
+        br.close();
+        bw.flush();
+        bw.close();
 
     }
 }
