77d3f108d30c17fa28875d8806250862961f0166
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-11 23:58:24 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
         System.out.println(" \\(__)|");
-
-        scanner.close();
 
     }
 }
