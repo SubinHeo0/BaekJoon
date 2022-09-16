cd7fd4c95888e0bac7ce578b7668e18369a6c8b6
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2022-09-17 01:43:46 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,46 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[10];
-        int cnt = 0; // 서로 다른 값 개수
-        int tmpCnt = 0;
+        HashSet<Integer> num = new HashSet<>();
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i=0; i<10; i++){
+            num.add(Integer.parseInt(br.readLine())%42);
+        }
 
-            arr[i] = Integer.parseInt(br.readLine()) % 42;
-            if (i == 0) {
-                cnt++;
-            } else {
-                tmpCnt = 0;
-                for (int j = 0; j < i; j++) {
-                    if (arr[i] != arr[j]) {
-                        tmpCnt++;
-                    } else { // 서로 같은 값이 있을때
-                        tmpCnt = 0;
-                        break;
-                    }
-                }
-                if (tmpCnt > 0) {
-                    cnt++;
-                }
-            } // else
-
-        } // for
-
-        bw.write(cnt + "");
+        System.out.println(num.size());
 
         br.close();
-        bw.flush();
-        bw.close();
 
     }
 }
