c43a9a83926d16b2e9c5ea8ffe999b57e472fd67
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2023-07-20 02:16:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1776 ms, Memory: 300880 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -12,20 +12,16 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int num = Integer.parseInt(br.readLine());
-        int[] arr = new int[num];
-        for (int i = 0; i < num; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
 
         // 계수 정렬(Counting Sort)
-        int[] count = new int[10001];
-        for (int i = 0; i < arr.length; i++) {
-            count[arr[i]]++;
+        int[] arr = new int[10001]; // 입력받은 값 = arr인덱스
+        for (int i = 0; i < num; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
         }
 
         // 출력
-        for (int i = 0; i < count.length; i++) {
-            int number = count[i];
+        for (int i = 0; i < arr.length; i++) {
+            int number = arr[i];
             while (number > 0) {
                 bw.write(i + "\n");
                 number--;
