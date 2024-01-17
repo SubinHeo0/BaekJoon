69812e6761c2b9a977d9fe4ff0d629a81e39fd50
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-01-18 01:33:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -8,28 +8,20 @@ public class Main {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        // 최대공약수: 유클리드 호제법
-        // MOD 연산을 나머지가 0이 나올 때까지 계속 수행
-        int gcd = 0; // 최대공약수
-        int first = num1;
-        int second = num2;
-        while (true) {
-            int[] nums = mod(first, second);
-            if (nums[1] == 0) {
-                gcd = nums[0];
-                break;
-            }
-            first = nums[0];
-            second = nums[1];
-
+        if (num2 > num1) {
+            int tmp = num2;
+            num2 = num1;
+            num1 = tmp;
         }
 
-        System.out.println(gcd);
-        System.out.println((num1 * num2) / gcd); // 최소공배수
+        int gcdResult = gcd(num1, num2); // 최대공약수
+        System.out.println(gcdResult);
+        System.out.println((num1 * num2) / gcdResult); // 최소공배수
 
     }
 
-    private static int[] mod(int first, int second) {
-        return new int[]{second, first % second}; // 작은값과 모드결과값 리턴
+    private static int gcd(int num1, int num2) {
+        if (num1 % num2 == 0) return num2;
+        return gcd(num2, num1 % num2);
     }
 }
