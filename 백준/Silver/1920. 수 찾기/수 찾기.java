cce8e8aa9d6a5b0bd073972d969416df4633417a
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-03-27 19:31:24 +0900

    [Silver IV] Title: 수 찾기, Time: 748 ms, Memory: 46220 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -9,29 +7,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        int[] cards = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            cards[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
+        Arrays.sort(cards);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            System.out.println(binarySearch(target, A, 0, A.length - 1));
+            int find = Integer.parseInt(st.nextToken());
+            bw.write(binarySearch(find, cards, 0, N - 1) + "\n");
         }
+
+        bw.flush();
 
     }
 
-    private static int binarySearch(int target, int[] arr, int start, int end) {
+    private static int binarySearch(int find, int[] cards, int start, int end) {
         if (start > end) return 0;
         int mid = start + ((end - start) / 2);
-        if (target == arr[mid]) return 1;
-        else if (target > arr[mid]) return binarySearch(target, arr, mid + 1, end);
-        else return binarySearch(target, arr, start, mid - 1);
+        if (find == cards[mid]) return 1;
+        else if (find > cards[mid]) return binarySearch(find, cards, mid + 1, end);
+        else return binarySearch(find, cards, start, mid - 1);
     }
 }
