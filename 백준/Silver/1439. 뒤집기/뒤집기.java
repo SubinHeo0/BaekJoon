1a26df125da5b09977be5cbf5900a81a9f9b5e0c
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-03-27 21:21:12 +0900

    [Silver V] Title: 뒤집기, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
@@ -10,20 +8,20 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] S = br.readLine().split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < S.length; i++) {
-            if (i == 0) {
-                map.put(S[i], 1);
-                continue;
-            }
-            if (!S[i].equals(S[i - 1])) {
-                map.put(S[i], map.getOrDefault(S[i], 0) + 1);
+        String str = br.readLine();
+        int zero = 0;
+        int one = 0;
+
+        if (str.charAt(0) == '0') zero++;
+        else one++;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                if (str.charAt(i) == '0') zero++;
+                else one++;
             }
         }
 
-        if (map.size() == 1) System.out.println(0);
-        else System.out.println(map.get("0") < map.get("1") ? map.get("0") : map.get("1"));
-
+        System.out.println(Math.min(zero, one));
     }
 }
