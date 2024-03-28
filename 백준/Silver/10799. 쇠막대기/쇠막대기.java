3924b8848bbbd317b2a773c3aed86a55bbc8f21f
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-03-28 19:25:45 +0900

    [Silver II] Title: 쇠막대기, Time: 176 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -9,21 +9,19 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        char[] input = br.readLine().toCharArray();
         Stack<Character> stack = new Stack<>();
-        int count = 0;
-        stack.push(input.charAt(0));
-        for (int i = 1; i < input.length(); i++) {
-            if (input.charAt(i) == ')') {
+        int answer = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == '(') stack.push(input[i]);
+            else {
                 stack.pop();
-                if (input.charAt(i - 1) == '(') count += stack.size(); // 레이저
-                else count += 1; // 레이저X
-            } else {
-                stack.push(input.charAt(i));
+                if (input[i - 1] == '(') answer += stack.size(); // 레이저
+                else answer += 1; // 레이저 X
             }
         }
 
-        System.out.println(count);
+        System.out.println(answer);
 
     }
 }
