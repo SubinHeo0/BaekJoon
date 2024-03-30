d0e4d223d916e7437ade879167dd7563189e0a20
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-03-31 04:53:08 +0900

    [Silver I] Title: 경로 찾기, Time: 272 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 플로이드-워셜 알고리즘
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if ((arr[i][k] == 1) && (arr[k][j] == 1)) arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+
+    }
+}
