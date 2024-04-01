63b248037231c42af85ceae459fa7d01be1eb367
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-02 04:14:40 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 552 ms, Memory: 47876 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import static java.lang.Math.*;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -25,24 +27,25 @@ public class Main {
     private static int level(int[] log) {
         Arrays.sort(log);
 
-        Queue<Integer> even = new LinkedList<>(); // 짝수 인덱스 값 모음
-        Stack<Integer> odd = new Stack<>(); // 홀수 인덱스 값 모음 | 나중에 뒤집어서 even 뒤에 이어붙여야하므로 stack으로 구현
-
-        for (int i = 0; i < log.length; i++) {
-            if (i % 2 == 0) even.add(log[i]);
-            else odd.add(log[i]);
-        }
-
-        for (int i = 0; i < log.length; i++) { // even 순서대로 출력후 odd 이어붙이기
-            if(!even.isEmpty()) log[i] = even.poll();
-            else log[i] = odd.pop();
+        int len = log.length;
+        int[] easy = new int[len];
+        int startIdx = 0;
+        int endIdx = len - 1;
+        for (int i = 0; i < len; i++) {
+            if (i % 2 == 0) {
+                easy[startIdx] = log[i];
+                startIdx++;
+            } else {
+                easy[endIdx] = log[i];
+                endIdx--;
+            }
         }
 
         // 최대 높이차 구하기
-        int max = Math.abs(log[0] - log[log.length - 1]);
-        for (int i = 0; i < log.length - 1; i++) {
+        int max = abs(easy[0] - easy[len - 1]);
+        for (int i = 0; i < len - 1; i++) {
             int j = i + 1;
-            if (Math.abs(log[i] - log[j]) > max) max = Math.abs(log[i] - log[j]);
+            if (abs(easy[i] - easy[j]) > max) max = abs(easy[i] - easy[j]);
         }
 
         return max;
