402b999078a43ddbfde5bc14843302128e20f507
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-01 21:06:07 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 724 ms, Memory: 52644 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
@@ -12,37 +10,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
-        int[][] point = new int[num][2];
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
 
-        for (int i = 0; i < point.length; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            // x좌표를 y좌표에, y좌표를 x좌표에 저장
-            point[i][1] = Integer.parseInt(st.nextToken());
-            point[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // Arrays.sort 람다식이용
-        Arrays.sort(point, (e1, e2) -> {
-            if (e1[0] == e2[0]) {
-                return e1[1] - e2[1];
-            } else {
-                return e1[0] - e2[0];
-            }
-        });
+        // y기준 정렬 | y가 같으면 x기준 정렬
+        Arrays.sort(arr, Comparator.comparingInt((int[] a) -> a[1])
+                .thenComparingInt(a -> a[0]));
 
-        // 출력
-        for (int i = 0; i < point.length; i++) {
-            // x y 좌표를 다시 원래대로 바꾸어서 출력
-            bw.write(point[i][1] + " " + point[i][0] + "\n");
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
 
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
 
     }
-
 }
