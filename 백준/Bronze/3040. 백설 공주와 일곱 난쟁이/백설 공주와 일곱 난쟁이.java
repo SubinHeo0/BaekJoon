63b2ad85fedbf022a8313a957dac99b639ac438b
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-02 16:58:13 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -9,34 +9,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] dwarf = new int[9];
-        int total = 0;
+        int sum = 0;
+
         for (int i = 0; i < 9; i++) {
-            dwarf[i] = Integer.parseInt(br.readLine());
-            total += dwarf[i];
+            int num = Integer.parseInt(br.readLine());
+            dwarf[i] = num;
+            sum += num;
         }
 
-        int key = total - 100; // 초과
+        int target = sum - 100;
         int spy1 = -1;
         int spy2 = -1;
-        boolean isExist = false;
         for (int i = 0; i < 9; i++) {
             for (int j = i + 1; j < 9; j++) {
-                if (dwarf[i] + dwarf[j] == key) {
-                    isExist = true;
-                    spy1 = j;
+                if (dwarf[i] + dwarf[j] == target) {
+                    spy1 = dwarf[i];
+                    spy2 = dwarf[j];
                     break;
                 }
             }
-            if (isExist) {
-                spy2 = i;
-                break;
-            }
+            if (spy1 != -1) break;
         }
 
-        // 스파이 빼고 출력
-        for (int i = 0; i < 9; i++) {
-            if ((i == spy1) || (i == spy2)) continue;
-            System.out.println(dwarf[i]);
+        for (int num : dwarf) {
+            if ((num != spy1) && (num != spy2)) System.out.println(num);
         }
 
     }
