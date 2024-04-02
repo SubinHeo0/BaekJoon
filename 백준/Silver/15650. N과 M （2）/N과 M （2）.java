1a684359cd0c5ce985b08710144eebc1ef6d1d0e
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-02 17:42:41 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -15,35 +15,33 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[M];
         visited = new boolean[N];
 
-        dfs(0);
+        dfs(0, 0);
         System.out.println(sb);
 
     }
 
-    private static void dfs(int depth) {
+    private static void dfs(int depth, int start) {
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(arr[i]).append(" ");
+            for (int i : arr) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = start; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 arr[depth] = i + 1;
-                dfs(depth + 1);
-                for (int j = i + 1; j < N; j++) {
-                    visited[j] = false;
-                }
+                dfs(depth + 1, i + 1);
+                visited[i] = false;
             }
         }
     }
+
 }
