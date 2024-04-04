2b35a24efb1f70d51b1a8197aac7d0944cdf5d18
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-05 08:54:09 +0900

    [Silver V] Title: 막대기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
@@ -10,32 +8,18 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int X = Integer.parseInt(br.readLine()); // 23
-        // 64로 만들 수 있는 나무 막대기 길이
-        List<Integer> bar = new ArrayList<>(); // 64 32 16 8 4 2 1
-        int num = 64;
-        while (num > 0) {
-            bar.add(num);
-            num /= 2;
-        }
-
-        int sum = 0;
-        int cnt = 0;
-        for (int i = 0; i < bar.size(); i++) {
-            int size = bar.get(i); // 8
-            if (size > X) continue;
-
-            if (sum + size <= X) {
-                sum += size; // 16
-                cnt++; // 1
-            } else {
-                continue;
+        int X = Integer.parseInt(br.readLine());
+        int start = 64;
+        int count = 0;
+        while (X > 0) {
+            if (start > X) start /= 2;
+            else {
+                X -= start;
+                count++;
             }
-
-            if (sum == X) break;
         }
 
-        System.out.println(cnt);
+        System.out.println(count);
 
     }
 }
