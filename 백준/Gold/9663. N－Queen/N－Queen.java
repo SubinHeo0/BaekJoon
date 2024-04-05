446d531ff0bb1f1a90377fdabe39cf14573b53b2
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-05 23:57:49 +0900

    [Gold IV] Title: N-Queen, Time: 4700 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,39 +4,40 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    private static int[] arr; //인덱스:열, 값:행
     private static int N;
+    private static int[] arr;
     private static int count = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        arr = new int[N]; // 인덱스:열, 값:행
 
         nQueen(0);
         System.out.println(count);
 
     }
 
-    private static void nQueen(int depth) { //depth가 열, i가 행
-        // 열을 다돌면 경우의 수 1 증가
+    private static void nQueen(int depth) { //depth:열
         if (depth == N) {
             count++;
             return;
         }
 
-        for (int i = 0; i < N; i++) { // 0번째 행부터 경우의 수 count 시작
-            arr[depth] = i;
-            // 놓을 수 있는 위치면 재귀 호출
-            if (isPosssible(depth)) nQueen(depth + 1); // 다음 열로 이동
+        // 첫 번째 행부터 가능한지 검사
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            if (isPossible(depth)) nQueen(depth + 1);
         }
+
     }
 
-    private static boolean isPosssible(int col) {
-        for (int i = 0; i < col; i++) {
-            if (arr[col] == arr[i]) return false; // 같은 행에 존재할 경우
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false; // 대각선에 존재할 경우
+    private static boolean isPossible(int col) { // 해당 열에 놓을 수 있는지 검사
+        for (int i = 0; i < col; i++) { // 이전 열(인덱스) 탐색 | 이전 인덱스에 같은 값이 있으면 안됨(같은 행), 대각선에 있으면 안됨
+            if (arr[i] == arr[col]) return false;
+            else if (col - i == Math.abs(arr[col] - arr[i])) return false;
         }
         return true;
     }
