fa5ca87a33cf6010ef57f188c32571f023b2c7e0
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-07 00:57:53 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 228 ms, Memory: 18852 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -21,15 +21,15 @@ public class Main {
 
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            return this.cost - o.cost; // 양수면 자리바꿈
         }
     }
 
     private static int N;
     private static int[][] cave;
     private static int[][] distance;
-    private static int[] dx = {1, 0, -1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {1, 0, -1, 0}; // 행
+    private static int[] dy = {0, 1, 0, -1}; // 열
 
     public static void main(String[] args) throws IOException {
 
@@ -42,36 +42,38 @@ public class Main {
             if (N == 0) break;
 
             cave = new int[N][N];
-            distance = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                Arrays.fill(distance[i], Integer.MAX_VALUE);
-            }
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     cave[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+
+            distance = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(distance[i], Integer.MAX_VALUE);
+            }
+            distance[0][0] = cave[0][0];
+
             sb.append("Problem ").append(cnt++).append(": ").append(dijkstra()).append("\n");
+
         }
         System.out.println(sb);
     }
 
     private static int dijkstra() {
-        distance[0][0] = cave[0][0];
-
         Queue<Node> q = new PriorityQueue<>();
-        q.add(new Node(0, 0, cave[0][0]));
+        q.offer(new Node(0, 0, cave[0][0]));
 
         while (!q.isEmpty()) {
             Node now = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextX = now.row + dx[i]; //행
-                int nextY = now.col + dy[i]; //열
+                int nextX = now.row + dx[i]; // 행
+                int nextY = now.col + dy[i]; // 열
 
-                if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < N) {
-                    if (distance[nextX][nextY] > distance[now.row][now.col] + cave[nextX][nextY]) {
+                if (0 <= nextX && nextX < N && 0 <= nextY && nextY < N) { //인접 값이 유효하면 distance 엽데이트 후 업데이트에 영향을 준 노드 큐에 추가
+                    if(distance[nextX][nextY] > distance[now.row][now.col] + cave[nextX][nextY]){
                         distance[nextX][nextY] = distance[now.row][now.col] + cave[nextX][nextY];
                         q.add(new Node(nextX, nextY, distance[nextX][nextY]));
                     }
