cc01b7c38c2c42fdc983ee7200e452039c9bab77
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-07 16:56:48 +0900

    [Gold V] Title: CCW, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.java
+++ 백준/Gold/11758. CCW/CCW.java
@@ -21,16 +21,12 @@ public class Main {
         int x3 = Integer.parseInt(st.nextToken());
         int y3 = Integer.parseInt(st.nextToken());
 
-        System.out.println(ccw(x1, y1, x2, y2, x3, y3));
-
-    }
-
-    private static int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
         int a = x1 * y2 + x2 * y3 + x3 * y1;
         int b = x2 * y1 + x3 * y2 + x1 * y3;
 
-        if (a - b > 0) return 1; // 반시계 방향
-        else if (a == b) return 0; // 일직선
-        else return -1; // 시계 방향
+        if (a - b > 0) System.out.println(1);
+        else if (a - b == 0) System.out.println(0);
+        else System.out.println(-1);
+
     }
 }
