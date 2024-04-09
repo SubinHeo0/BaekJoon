ccccf6eb8b720ff207243d1a144fd99f99978a09
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-10 03:25:21 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -12,13 +12,18 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
         int[] dp = new int[k + 1];
         dp[0] = 1;
 
         for (int i = 0; i < n; i++) {
-            int coin = Integer.parseInt(br.readLine());
-            for (int j = coin; j <= k; j++) {
-                dp[j] = dp[j] + dp[j - coin];
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = dp[i] + dp[i - coin];
             }
         }
 
