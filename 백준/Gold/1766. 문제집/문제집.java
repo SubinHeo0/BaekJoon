24dac19a284213ca9155df10babd52a9d945b12b
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-13 07:23:47 +0900

    [Gold II] Title: 문제집, Time: 612 ms, Memory: 50956 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -8,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -19,35 +17,31 @@ public class Main {
             graph.add(new ArrayList<>());
         }
 
-        int[] inputCnt = new int[N + 1]; // 전입차수 저장
+        int[] indegree = new int[N + 1];
         for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
             int first = Integer.parseInt(st.nextToken());
             int last = Integer.parseInt(st.nextToken());
 
             graph.get(first).add(last);
-            inputCnt[last] += 1;
+            indegree[last]++;
         }
 
-        // 위상정렬 우선순위큐
-        // 전입차수가 0인 문제 큐에 추가
         Queue<Integer> q = new PriorityQueue<>();
         for (int i = 1; i <= N; i++) {
-            if (inputCnt[i] == 0) q.offer(i);
+            if (indegree[i] == 0) q.offer(i);
         }
 
         while (!q.isEmpty()) {
             Integer first = q.poll();
-            sb.append(first).append(" ");
-            if (!graph.get(first).isEmpty()) {
-                for (Integer last : graph.get(first)) { // first에 인접한 값의 진입차수 감소
-                    inputCnt[last]--;
-                    if (inputCnt[last] == 0) q.offer(last);
-                }
+            bw.write(first + " ");
+            for (Integer last : graph.get(first)) {
+                indegree[last]--;
+                if (indegree[last] == 0) q.offer(last);
             }
         }
 
-        System.out.println(sb);
+        bw.flush();
 
     }
 }
