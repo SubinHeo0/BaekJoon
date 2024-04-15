069e13244b83fb4eec07346bd7b6320d7623f44f
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-16 06:18:42 +0900

    [Gold II] Title: 트리, Time: 448 ms, Memory: 39384 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -15,8 +15,8 @@ public class Main {
         int testCase = Integer.parseInt(br.readLine());
         for (int i = 0; i < testCase; i++) {
             n = Integer.parseInt(br.readLine());
-            pre = new int[n];
-            in = new int[n];
+            pre = new int[n + 1];
+            in = new int[n + 1];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 pre[j] = Integer.parseInt(st.nextToken());
@@ -25,7 +25,7 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 in[j] = Integer.parseInt(st.nextToken());
             }
-            postOrder(0, 0, n - 1);
+            postOrder(0, 0, n);
             bw.write("\n");
         }
 
@@ -34,19 +34,12 @@ public class Main {
     }
 
     private static void postOrder(int root, int start, int end) throws IOException {
-        if (root >= n) return;
-
         int rootValue = pre[root];
 
-        if (start == end) { // 그룹에 원소가 하나만 남으면 바로 출력 후 종료
-            bw.write(rootValue + " ");
-            return;
-        }
-
         // 중위 순회 범위 탐색
-        for (int i = start; i <= end; i++) {
+        for (int i = start; i < end; i++) {
             if (in[i] == rootValue) {
-                postOrder(root + 1, start, i - 1); // 왼쪽
+                postOrder(root + 1, start, i); // 왼쪽
                 postOrder(root + (i - start) + 1, i + 1, end); // 오른쪽
                 bw.write(rootValue + " "); // 루트
                 return;
