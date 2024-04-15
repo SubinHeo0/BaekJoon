c8d25449ee1b4d0c77983b0dc8849162637ed0ce
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-16 00:14:41 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 648 ms, Memory: 69704 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,22 +1,21 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    private static List<List<Integer>> graph = new ArrayList<>();
-    private static int[] parent;
+    private static int N;
     private static boolean[] visit;
+    private static int[] parent;
+    private static List<List<Integer>> graph = new ArrayList<>();
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        parent = new int[N + 1];
+        N = Integer.parseInt(br.readLine());
         visit = new boolean[N + 1];
+        parent = new int[N + 1];
 
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
@@ -24,13 +23,14 @@ public class Main {
 
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            graph.get(v1).add(v2);
-            graph.get(v2).add(v1);
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            graph.get(num1).add(num2);
+            graph.get(num2).add(num1);
         }
 
-        dfs(1); // 루트:1
+        dfs(1); // 부모 저장
 
         for (int i = 2; i <= N; i++) {
             bw.write(parent[i] + "\n");
@@ -42,10 +42,10 @@ public class Main {
 
     private static void dfs(int idx) {
         visit[idx] = true;
-        for (int i : graph.get(idx)) {
-            if (!visit[i]) {
-                parent[i] = idx;
-                dfs(i);
+        for (int neighbor : graph.get(idx)) {
+            if (!visit[neighbor]) {
+                parent[neighbor] = idx;
+                dfs(neighbor);
             }
         }
     }
