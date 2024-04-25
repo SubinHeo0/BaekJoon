2aa20d55d7a7a602b7677fa7748db94f5f6409aa
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-25 23:43:52 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 140 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= cnt; i++) { // 1 2 3
+            for (int j = 0; j < cnt - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        for (int i = cnt - 1; i > 0; i--) { // 2 1
+            for (int j = 0; j < cnt - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+
+    }
+}
