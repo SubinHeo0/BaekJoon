490c11c7b98399dad20a10c442b676b3c1a64a0e
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-25 21:14:48 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int line = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < line; i++) {
-            for (int j = 0; j <= i; j++) {
-                bw.write('*');
+        int cnt = Integer.parseInt(br.readLine());
+        char star = '*';
+        for (int i = 1; i <= cnt; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write(star);
             }
-            bw.newLine();
+            bw.write("\n");
         }
 
-        br.close();
         bw.flush();
-        bw.close();
 
     }
 }
