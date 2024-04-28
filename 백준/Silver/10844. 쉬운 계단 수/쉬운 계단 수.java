b058b4e9c524eb072f7da35fe9f6fda38866c28b
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-28 20:32:12 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -4,40 +4,35 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    private final static long MOD = 1000000000L;
-    private static long[][] dp;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        long MOD = 1000000000L;
         int N = Integer.parseInt(br.readLine());
-        dp = new long[N + 1][10]; //[자릿수][값(0~9)] 경우의 수 저장
+        long[][] dp = new long[N + 1][10]; //[자릿수][값(0~9)] 경우의 수 저장
 
         // 자릿수가 1이면 1로 초기화
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 10; i++) {
             dp[1][i] = 1L;
         }
 
-        // 마지막 자릿수(제일 왼쪽 값)인 1~9까지의 경우의 수 더하기
+        // 두 번째 자릿수부터 N(제일 왼쪽 값)번째 자릿수까지 탐색
+        for (int i = 2; i <= N; i++) {
+            for (int val = 0; val < 10; val++) { // 자릿값 탐색
+                if (val == 0) dp[i][val] = dp[i - 1][1] % MOD;
+                else if (val == 9) dp[i][val] = dp[i - 1][8] % MOD;
+                else dp[i][val] = (dp[i - 1][val - 1] + dp[i - 1][val + 1]) % MOD;
+            }
+        }
+
+        // 마지막 자릿수의 자릿값 경우의 수 더하기
         long sum = 0L;
-        for (int i = 1; i <= 9; i++) {
-            sum += recur(N, i);
+        for (int val = 0; val < 10; val++) {
+            sum += dp[N][val];
         }
 
         System.out.println(sum % MOD);
 
-    }
-
-    private static long recur(int digit, int val) { // digit:자릿수, val:값
-        if (digit == 1) return dp[digit][val];
-
-        if (dp[digit][val] == 0L) {
-            if (val == 0) dp[digit][val] = recur(digit - 1, 1);
-            else if (val == 9) dp[digit][val] = recur(digit - 1, 8);
-            else dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
-        }
-
-        return dp[digit][val] % MOD;
     }
 }
