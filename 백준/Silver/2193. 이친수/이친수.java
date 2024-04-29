babf14dab081835cb8c010ccf98bfb6afd88dfa9
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-29 22:50:30 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -10,13 +10,10 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         long[][] dp = new long[N + 1][2]; // [자릿수][값(0,1)]
-        dp[1][1] = 1L; // dp[1][0] = 0(0으로 시작X)
-        for (int i = 2; i <= N; i++) { // 자릿수
-            if (dp[i - 1][0] != 0) {
-                dp[i][0] += dp[i - 1][0];
-                dp[i][1] += dp[i - 1][0];
-            }
-            if (dp[i - 1][1] != 0) dp[i][0] += dp[i - 1][1];
+        dp[1][1] = 1L;
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
         }
 
         System.out.println(dp[N][0] + dp[N][1]);
