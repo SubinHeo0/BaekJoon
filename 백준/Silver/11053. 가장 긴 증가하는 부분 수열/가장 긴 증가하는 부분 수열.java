48200660f927c5b998d792ce60f2e9a2f94113e4
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-30 04:28:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -10,26 +10,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
+        int[] A = new int[N + 1];
+        int[] dp = new int[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-
-            int max = 0;
-            for (int j = 0; j < i; j++) { // 전에 저장된 값들 중 현재 입력값보다 작은 값의 최대 dp구하기
-                if (arr[j] < num) {
-                    max = Math.max(max, dp[j]);
-                }
-            }
-            dp[i] = max + 1;
-            answer = Math.max(answer, dp[i]);
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(answer);
+        for (int i = 1; i <= N; i++) { // 이전의 값들중 현재 값보다 작은 수의 최대 dp를 가져와서 +1
+            int max = dp[0];
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i]) max = Math.max(max, dp[j]);
+            }
+            dp[i] = max + 1;
+        }
+
+        int max = dp[0];
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
 
     }
 }
