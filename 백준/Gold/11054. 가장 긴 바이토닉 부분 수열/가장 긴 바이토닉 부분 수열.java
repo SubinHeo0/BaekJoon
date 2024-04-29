9af2557543a4363f873b8f140ddd3c023805f859
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-04-30 08:09:06 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 160 ms, Memory: 15244 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N + 1];
+        int[] LR = new int[N + 1]; // -> (순방향 LIS)
+        int[] RL = new int[N + 1]; // <- (역방향 LIS)
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 순방향 LIS
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i]) max = Math.max(max, LR[j]);
+            }
+            LR[i] = max + 1;
+        }
+
+        // 역방향 LIS
+        RL[N] = 1;
+        for (int i = N - 1; i >= 1; i--) {
+            int max = 0;
+            for (int j = i + 1; j <= N; j++) {
+                if (A[j] < A[i]) max = Math.max(max, RL[j]);
+            }
+            RL[i] = max + 1;
+        }
+
+        // 두 배열 더하기
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            answer = Math.max(answer, LR[i] + RL[i] - 1);
+        }
+
+        System.out.println(answer);
+
+    }
+}
