8b85862396549105503deb7e74e8197d0cff0d48
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-06 01:10:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 748 ms, Memory: 49912 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,38 +9,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = Integer.parseInt(br.readLine());
-        int[][] point = new int[num][2];
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
 
-        for (int i = 0; i < point.length; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            point[i][0] = Integer.parseInt(st.nextToken());
-            point[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // point[i][0] 과 point[i+1][0] 정렬
-        // 만약 두 값이 같다면 point[i][1] 과 point[i+1][1]을 비교하여 정렬
-        // 2차원배열을 매개변수로 받는 Arrays.sort에서 T는 int[]
-        Arrays.sort(point, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] e1, int[] e2) {
-                if (e1[0] == e2[0]) { // 첫번째 원소가 같다면 두 번째 원소끼리 비교
-                    return e1[1] - e2[1];
-                } else {
-                    return e1[0] - e2[0];
-                }
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
             }
         });
 
-        // 출력
-        for (int i = 0; i < point.length; i++) {
-            bw.write(point[i][0] + " " + point[i][1] + "\n");
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
         }
 
-        br.close();
         bw.flush();
-        bw.close();
 
     }
-
 }
