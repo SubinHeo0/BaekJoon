765b15f27623bfe8ceab10c337c908675b8075c1
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-21 04:08:54 +0900

    [Bronze I] Title: 최소공배수, Time: 176 ms, Memory: 15104 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A < B) {
+                int tmp = A;
+                A = B;
+                B = tmp;
+            }
+
+            int D = gcd(A, B); // 최대공약수
+            bw.write((A * B) / D + "\n");
+        }
+
+        bw.flush();
+
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
