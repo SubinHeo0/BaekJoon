d432a2112c8b408b9db759abe7ec0aabd6c8a297
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-20 12:09:43 +0900

    [Silver IV] Title: 괄호, Time: 140 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char[] input = st.nextToken().toCharArray();
+
             Stack<Character> stack = new Stack<>();
-            boolean isVPS = true;
-            for (int j = 0; j < input.length(); j++) {
-                if (input.charAt(j) == '(') stack.push(input.charAt(j));
+            String answer = "YES";
+            for (char c : input) {
+                if (c == '(') stack.push(c);
                 else {
                     if (stack.isEmpty()) {
-                        isVPS = false;
+                        answer = "NO";
                         break;
-                    } else {
-                        stack.pop();
-                    }
+                    } else stack.pop();
                 }
             }
-            if (!stack.isEmpty()) isVPS = false;
-            System.out.println(isVPS ? "YES" : "NO");
+            if (!stack.isEmpty()) answer = "NO";
+            bw.write(answer + "\n");
         }
+
+        bw.flush();
 
     }
 }
