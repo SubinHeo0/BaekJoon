137969458be9b11a8f549b7e0981e1770466416a
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-20 13:39:38 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 144 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.LinkedList;
+
+public class Main {
+
+    private static LinkedList<Integer> deque = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] alphabet = new int[26];
+        char[] input = br.readLine().toCharArray();
+        for (char c : input) {
+            alphabet[c - 97]++;
+        }
+
+        for (int cnt : alphabet) {
+            bw.write(cnt + " ");
+        }
+
+        bw.flush();
+
+    }
+}
