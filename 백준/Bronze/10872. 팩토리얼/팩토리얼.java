9e7070366aae95db69ade015011417ce6577f02c
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-25 03:32:41 +0900

    [Bronze III] Title: 팩토리얼, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -14,8 +14,7 @@ public class Main {
     }
 
     private static int fact(int n) {
-        if (n == 0) return 1;
-        if (n == 1) return 1;
+        if (n <= 1) return 1;
         return n * fact(n - 1);
     }
 }
