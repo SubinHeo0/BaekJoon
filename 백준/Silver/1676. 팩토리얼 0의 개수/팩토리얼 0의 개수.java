bca5afcb0eda1ccb48643260bd1a0664362eee4a
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-05-25 19:01:16 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -9,13 +9,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
 
+        int answer = 0;
         while (N >= 5) {
-            cnt += N / 5;
+            answer += N / 5;
             N /= 5;
         }
 
-        System.out.println(cnt);
+        System.out.println(answer);
+
     }
 }
