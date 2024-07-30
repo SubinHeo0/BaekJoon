0c95a4ef4d176413a7a192000b7839fac94283a7
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-07-31 01:07:14 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,26 +1,49 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int testCase = Integer.parseInt(br.readLine());
-        for (int i = 0; i < testCase; i++) {
+        int T = Integer.parseInt(br.readLine());
+        int[] dp;
+
+        for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] dp = new int[11];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            for (int j = 4; j <= n; j++) {
-                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
-            }
-            bw.write(dp[n] + "\n");
-        }
+            dp = new int[n + 1]; // n이 4보다 작은 경우도 생각해줘야 함
 
-        bw.flush();
+            if (n == 1) {
+                System.out.println(1);
+                continue;
+            }
+
+            if (n == 2) {
+                System.out.println(2);
+                continue;
+            }
+
+            if (n == 3) {
+                System.out.println(4);
+                continue;
+            }
+
+            if (n >= 4) {
+                dp[1] = 1;
+                dp[2] = 2;
+                dp[3] = 4;
+
+                for (int j = 4; j <= n; j++) {
+                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+                }
+
+                System.out.println(dp[n]);
+            }
+
+
+        }
 
     }
 }
