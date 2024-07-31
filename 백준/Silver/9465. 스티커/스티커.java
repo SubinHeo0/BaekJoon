f58317ab406706494634a17078d48d3ad510786e
Subin Heo <86907165+SubinHeo0@users.noreply.github.com>
2024-08-01 02:53:28 +0900

    [Silver I] Title: 스티커, Time: 652 ms, Memory: 131720 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,39 +1,37 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int T = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[][] arr = new int[2][n + 1];
-            int[][] dp = new int[2][n + 1];
+            int[][] cost = new int[2][n + 1];
 
             for (int row = 0; row < 2; row++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
+                String[] tmp = br.readLine().split(" ");
                 for (int col = 1; col <= n; col++) {
-                    arr[row][col] = Integer.parseInt(st.nextToken());
+                    cost[row][col] = Integer.parseInt(tmp[col - 1]);
                 }
             }
 
-            dp[0][1] = arr[0][1];
-            dp[1][1] = arr[1][1];
+            int[][] dp = new int[2][n + 1];
+            dp[0][1] = cost[0][1];
+            dp[1][1] = cost[1][1];
 
             for (int j = 2; j <= n; j++) {
-                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + arr[0][j];
-                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + arr[1][j];
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + cost[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + cost[1][j];
             }
 
-            bw.write(Math.max(dp[0][n], dp[1][n]) + "\n");
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
 
         }
-
-        bw.flush();
 
     }
 }
